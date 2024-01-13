bb9f93ad546b4820c99b35b6f631fa2942735952
shahriarmohim-bs23 <nasifshahriar4@gmail.com>
2024-01-13 13:56:04 +0600

    database_configuration_updated

--- travel_helper/settings.py
+++ travel_helper/settings.py
@@ -89,17 +89,24 @@ WSGI_APPLICATION = 'travel_helper.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': os.environ.get('POSTGRES_DB', 'postgres'),
+#         'USER': os.environ.get('POSTGRES_USER', 'postgres'),
+#         'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'postgres'),
+#         'HOST': os.environ.get('POSTGRES_HOST', 'postgres'),
+#         'PORT': os.environ.get('POSTGRES_PORT', 5432),
+#         'OPTIONS': {
+#             'connect_timeout': 25,
+#         }
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('POSTGRES_DB', 'postgres'),
-        'USER': os.environ.get('POSTGRES_USER', 'postgres'),
-        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'postgres'),
-        'HOST': os.environ.get('POSTGRES_HOST', 'postgres'),
-        'PORT': os.environ.get('POSTGRES_PORT', 5432),
-        'OPTIONS': {
-            'connect_timeout': 25,
-        }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME':  'db.sqlite3'
     }
 }
 
